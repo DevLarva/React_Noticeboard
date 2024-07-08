641d6be9770251e97efa5a8ee0ce555bba4131e3
백대홍 <bdh3620@naver.com>
2024-07-08 17:42:58 +0900

    Feat: 작성글 0개 일때 뷰처리

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Typography, Box } from '@mui/material';
 
 export default function PostList({ posts }) {
     return (
@@ -14,16 +14,28 @@ export default function PostList({ posts }) {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {posts.map((post, index) => (
-                        <TableRow key={index}>
-                            <TableCell sx={{ width: '10%' }}>{index + 1}</TableCell>
-                            <TableCell sx={{ width: '50%' }}>{post.title}</TableCell>
-                            <TableCell sx={{ width: '20%' }}>{post.author}</TableCell>
-                            <TableCell sx={{ width: '20%' }}>{post.date}</TableCell>
+                    {posts.length === 0 ? (
+                        <TableRow>
+                            <TableCell colSpan={4}>
+                                <Box sx={{ textAlign: 'center', padding: 2 }}>
+                                    <Typography variant="h6" fontWeight="bold">
+                                        아직 작성한 글이 없습니다.
+                                    </Typography>
+                                </Box>
+                            </TableCell>
                         </TableRow>
-                    ))}
+                    ) : (
+                        posts.map((post, index) => (
+                            <TableRow key={index}>
+                                <TableCell sx={{ width: '10%' }}>{index + 1}</TableCell>
+                                <TableCell sx={{ width: '50%' }}>{post.title}</TableCell>
+                                <TableCell sx={{ width: '20%' }}>{post.author}</TableCell>
+                                <TableCell sx={{ width: '20%' }}>{post.date}</TableCell>
+                            </TableRow>
+                        ))
+                    )}
                 </TableBody>
             </Table>
         </TableContainer>
     );
-};
+}
